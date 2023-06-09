960618224e4bea2d2772d4e269fa0802eee7359c
Munawwar <munawwarfiroz@hotmail.com>
2023-06-09 16:21:06 +0400

    fast paths

--- frameworks/keyed/domdiff-alt/src/index.js
+++ frameworks/keyed/domdiff-alt/src/index.js
@@ -6,14 +6,60 @@ function domdiff(
   currentNodes = parentNode.childNodes, // Array of current items/nodes
   futureNodes = [], // Array of future items/nodes
 ) {
+  // fast path for full replacement of content
+  let atLeastOneMatch = false;
+  for (let i = 0, len = Math.min(currentNodes.length, futureNodes.length); i < len; i++) {
+    if (currentNodes[i] === futureNodes[i]) {
+      atLeastOneMatch = true;
+      break;
+    }
+  }
+  if (!atLeastOneMatch) {
+    parentNode.replaceChildren(...futureNodes);
+    return;
+  }
+
   // remove all nodes not in futureNodes
-  const futureNodesSet = new Set(futureNodes);
+  const futureNodesIndexMap = new Map();
+  futureNodes.forEach((node, index) => futureNodesIndexMap.set(node, index));
   // remove in reverse order, so that live child nodes removal doesn't affect iteration.
   for (let i = currentNodes.length - 1; i >= 0; i--) {
     let node = currentNodes[i];
-    if (!futureNodesSet.has(node)) {
+    if (!futureNodesIndexMap.has(node)) {
       parentNode.removeChild(node);
     }
+  }
+
+  // Optimize for swaps
+  let childNodes = parentNode.childNodes;
+  if (childNodes.length === futureNodes.length) {
+    // if lists are equal, then no need to waste memory
+    if (futureNodes.every((node, index) => node === childNodes[index])) return;
+    let currentNodesIndexMap = new Map();
+    for (let i = 0; i < childNodes.length - 1; i++) {
+      currentNodesIndexMap.set(childNodes[i], i);
+    }
+    let swaps = [];
+    let swapLookup = new Set();
+    for (let i = 0; i < childNodes.length - 1; i++) {
+      let currentNode = childNodes[i];
+      let futureNode = futureNodes[i];
+      if (currentNode == futureNode || swapLookup.has(futureNode)) continue;
+      let finalIndexOfCurrentNode = futureNodesIndexMap.get(currentNode);
+      let currentIndexOfFutureNode = currentNodesIndexMap.get(futureNode);
+      if (
+        finalIndexOfCurrentNode === currentIndexOfFutureNode
+        && finalIndexOfCurrentNode > -1
+        && currentIndexOfFutureNode > -1
+      ) {
+        swaps.push([currentNode, futureNode, i]);
+      }
+    }
+
+    swaps.forEach(([currentNode, futureNode, currentNodeIndex]) => {
+      parentNode.insertBefore(currentNode, futureNode);
+      parentNode.insertBefore(futureNode, childNodes[currentNodeIndex]);
+    });
   }
 
   // insert the future nodes into position
